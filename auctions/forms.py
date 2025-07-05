22f46a34c5a7b94537a037855ffef66d863d4e37
Sahalu Aminu <sahlminkok7@gmail.com>
2025-07-05 13:02:16 +0100

    Style the app with bootstrap

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,7 +1,14 @@
-from django.forms import ModelForm
+from django import forms
 from .models import AuctionListing
 
-class AuctionListingForm(ModelForm):
+class AuctionListingForm(forms.ModelForm):
     class Meta:
         model = AuctionListing
         fields = ["title", "description", "starting_bid", "image"]
+
+        widgets = {
+            "title": forms.TextInput(attrs={ "class": "form-control" }),
+            "description": forms.Textarea(attrs={ "class": "form-control" }),
+            "starting_bid": forms.NumberInput(attrs={ "class": "form-control" }),
+            "image": forms.URLInput(attrs={ "class": "form-control" })
+        }
