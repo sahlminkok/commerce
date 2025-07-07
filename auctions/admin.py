f5f157fa7bbf63032944426d64e9060ad8b8c326
Sahalu Aminu <sahlminkok7@gmail.com>
2025-07-07 12:05:15 +0100

    Register WatchlitItem to admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, AuctionListing, Bid, Comment
+from .models import User, AuctionListing, Bid, Comment, WatchlistItem
 
 admin.site.register(User)
 admin.site.register(AuctionListing)
 admin.site.register(Bid)
 admin.site.register(Comment)
+admin.site.register(WatchlistItem)
